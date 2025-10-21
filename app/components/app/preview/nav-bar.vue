c18717a6faccfb52a6b698d140fbdbc03407661e
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-10-21 13:32:21 -0400

    :sparkles: setup preview layout and page (#11)

---
+++ app/components/app/preview/nav-bar.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <menu>
+      <li>
+        <NuxtLink to="/links">
+          Back to Editor
+        </NuxtLink>
+      </li>
+      <li>
+        <button>
+          Share Link
+        </button>
+      </li>
+    </menu>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: var(--space-200) var(--space-300);
+}
+
+menu {
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+}
+
+menu > li {
+  list-style-type: none;
+}
+
+/* viewport: mobile -> tablet */
+@media (min-width: 45rem) {
+  header {
+    padding: var(--space-300);
+    background-color: hsl(var(--color-purple-600));
+    border-end-start-radius: var(--br-800);
+    border-end-end-radius: var(--br-800);
+  }
+
+  menu {
+    background-color: hsl(var(--color-white));
+    padding: var(--space-200) var(--space-300);
+    border-radius: var(--br-500);
+  }
+}
+</style>
