1ff742bc1b95a0912ddb6669cadab5ca79472229
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-10-21 13:30:54 -0400

    :sparkles: setup preview layout and page

--- app/layouts/preview.vue
+++ app/layouts/preview.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    <header>
-      <img
-        src="/logo-devlinks-large.svg"
-        alt="Devlinks logo"
-        width="183"
-        height="40"
-      >
-    </header>
+  <div class="container">
+    <AppPreviewNavBar />
     <main>
       <slot />
     </main>
   </div>
 </template>
+
+<style scoped>
+.container {
+  min-block-size: 100vb;
+  max-inline-size: var(--editor-inline-size-max);
+  margin-inline: auto;
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+
+main {
+  margin-inline: auto;
+  margin-block: var(--space-300);
+}
+
+/* viewport: mobile -> tablet */
+@media (min-width: 45rem) {
+  .container {
+    grid-template-rows: 136px 221px 1fr;
+  }
+
+  .container > :first-child {
+    grid-column: 1 / -1;
+    grid-row: 1 / 3;
+  }
+
+  .container > :last-child {
+    grid-column: 1 / -1;
+    grid-row: 2 / -1;
+  }
+}
+</style>
--- app/pages/preview.vue
+++ app/pages/preview.vue
@@ -5,9 +5,24 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    Back to Editor
-
-    Share Link
+  <div class="container">
+    <img
+      src="/illustration-phone-mockup.svg"
+      alt="Illustration phone mockup"
+      width="308"
+      height="632"
+    >
   </div>
 </template>
+
+<style scoped>
+/* viewport: mobile -> tablet */
+@media (min-width: 45rem) {
+  .container {
+    background-color: hsl(var(--color-white));
+    border-radius: var(--br-600);
+    padding: var(--space-600) var(--space-700);
+    box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
