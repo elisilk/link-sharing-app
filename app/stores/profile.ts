d2a3047476f7520578d091179e05ac7a75c9285a
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-10-30 12:33:42 -0400

    :sparkles: improve preview component (#43)

--- app/stores/profile.ts
+++ app/stores/profile.ts
@@ -7,12 +7,14 @@ export type link = {
 };
 
 export const useProfileStore = defineStore("profileStore", () => {
-  const firstName = ref<string | null>("Eli");
-  const lastName = ref<string | null>("Silk");
-  const email = ref<string | null>("esilk@me.com");
+  const firstName = ref<string | null>("Ben");
+  const lastName = ref<string | null>("Wright");
+  const email = ref<string | null>("ben@example.com");
   const links = ref<link[]>([
-    { order: 1, platform: "GitHub", url: "https://github.com/elisilk/" },
-    { order: 2, platform: "Frontend Mentor", url: "https://www.frontendmentor.io/profile/elisilk" },
+    { order: 1, platform: "GitHub", url: "https://github.com/benwright/" },
+    { order: 2, platform: "YouTube", url: "https://www.youtube.com/@benwright" },
+    { order: 3, platform: "LinkedIn", url: "https://www.linkedin.com/in/benwright/" },
+    /* { order: 4, platform: "Frontend Mentor", url: "https://www.frontendmentor.io/profile/elisilk" }, */
   ]);
 
   return {
