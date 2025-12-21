7b4c003811ba0f3ebfac04b4bc78d82a74cba5e9
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-12-21 17:43:41 -0500

    Set up supabase database with basic auth

--- supabase/migrations/20251212185832_profiles-schema.sql
+++ supabase/migrations/20251212185832_profiles-schema.sql
@@ -1,19 +1,30 @@
-drop table if exists profiles;
+drop table if exists public.profiles;
 TRUNCATE auth.users cascade;
 
 -- Create the table
 create table 
-  profiles (
+  public.profiles (
     id uuid references auth.users on delete cascade not null,
     created_at timestamptz default now() not null,
     first_name text not null,
     last_name text not null,
+    email text not null,
     
     primary key (id)
   );
 
-alter table profiles enable row level security;
+-- Enable RLS
+alter table public.profiles enable row level security;
 
-create policy "Enable read access for all users"
-on "public"."profiles"
-for select using (true);
+-- Create policies
+create policy "Public profiles are visible to everyone."
+on public.profiles
+for select
+to authenticated, anon
+using ( true );
+
+create policy "Public profiles can be created by everyone."
+on public.profiles
+for insert
+to authenticated, anon
+with check ( true );
