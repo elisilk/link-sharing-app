88c3e39a6ecc91931040c7ce0f507b141e23cb0d
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-12-31 12:44:43 -0500

    :sparkles: typesafe env setup

--- app/pages/editor.vue
+++ app/pages/editor.vue
@@ -5,5 +5,39 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>Editor</h1>
+  <div class="container mx-auto mt-4">
+    <h1 class="sr-only">
+      Editor
+    </h1>
+
+    <div class="tabs tabs-lift">
+      <label class="tab flex align-middle gap-1">
+        <input
+          type="radio"
+          name="editor_tabs"
+          checked
+        >
+        <Icon name="tabler:link" size="18" />
+        Links
+      </label>
+      <div class="tab-content bg-base-100 border-base-300 p-6">
+        <h2 class="text-3xl font-bold">
+          Customize your links
+        </h2>
+        <p>Add/edit/remove links below and then share all your profiles with the world!</p>
+      </div>
+
+      <label class="tab flex align-middle gap-1">
+        <input type="radio" name="editor_tabs">
+        <Icon name="tabler:user-circle" size="18" />
+        Profile Details
+      </label>
+      <div class="tab-content bg-base-100 border-base-300 p-6">
+        <h2 class="text-3xl font-bold">
+          Profile Details
+        </h2>
+        <p>Add your details to create a personal touch to your profile.</p>
+      </div>
+    </div>
+  </div>
 </template>
--- app/pages/preview.vue
+++ app/pages/preview.vue
@@ -6,6 +6,10 @@ definePageMeta({
 
 <template>
   <div class="container mx-auto mt-4 flex justify-center">
+    <h1 class="sr-only">
+      Preview
+    </h1>
+
     <div class="mockup-phone">
       <div class="mockup-phone-camera" />
       <div class="mockup-phone-display text-white grid place-content-center bg-neutral-900">
