edb929ee740228c749acffa9bc0c10c4460bad6b
Eli Silk <37377295+elisilk@users.noreply.github.com>
2026-01-02 16:29:16 -0500

    :sparkles: removed better auth

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -7,8 +7,6 @@ const EnvSchema = z.object({
   NUXT_SESSION_PASSWORD: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
-  BETTER_AUTH_URL: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
